4d3cf21b11b77c6de411d0d9c6059a058898483e
Manvi <143207135+Mnv31@users.noreply.github.com>
2025-02-09 17:00:33 +0530

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,56 @@
+package shapes;
+public interface Shape {
+    double area();
+    double perimeter();
+}
+package shapes;
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+package shapes;
+public class Rectangle implements Shape {
+    private double length, width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+}
+import shapes.Circle;
+import shapes.Rectangle;
+import shapes.Shape;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5.0);
+        Shape rectangle = new Rectangle(4.0, 6.0);
+
+        System.out.println("Circle Area: " + circle.area());
+        System.out.println("Circle Perimeter: " + circle.perimeter());
+
+        System.out.println("Rectangle Area: " + rectangle.area());
+        System.out.println("Rectangle Perimeter: " + rectangle.perimeter());
+    }
+}
